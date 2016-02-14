847162af15473a763ac7614bfbd5e60cec485365
Matthew Wong <matthewwong3.14@gmail.com>
2016-02-13 16:56:09 -0800

    initial table creation on history page

--- application/views/pages/history.php
+++ application/views/pages/history.php
@@ -1,8 +1,27 @@
 <div id="header-featured">
-	<div id="banner-wrapper">
-		<div id="banner" class="container">
-			<h2><? echo $title; ?></h2>
+    <h2>History:</h2>
+        <table class="table">
+            <tr>
+                <th>Purchase/Sell</th>
+                <th>Price per Stock</th>
+                <th>Amount of Stocks</th>
+                <th>Total Price</th>
+                <th>Date/Time</th>
+            </tr>
+            {}
+            <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>{}</td>
+                <td>{}</td>
+                <td>{}</td>
+            </tr>
+            {}
+        </table>
+    <div id="banner-wrapper">
+        <div id="banner" class="container">
+            <h2><? echo $title; ?></h2>
 			
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
